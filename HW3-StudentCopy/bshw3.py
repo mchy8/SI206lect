be77e4d335f154345edb3614dfb3ee2c66cffd58
Dr Brown <Dr Brown>
2016-11-03 01:17:11 -0400

    2nd commit

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -7,6 +7,19 @@
 # 2) Replace the main picture with a picture of yourself.
 # 3) Replace any local images with the image I provided in media.  (You
 # must keep the image in a separate folder than your html code.
-
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+import urllib.request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+
+
+url = "https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions"
+r = requests.get(url)
+html = urlopen(url).read()
+soup = BeautifulSoup(html, "html.parser")
+
+for word in soup.find_all(class_="field-item"):
+	
--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -3,13 +3,69 @@
 
 # Requirements:
 # 1) Only use the first 150 tokens
-# 2) Pick 5 parts of speech to prompt for, including nouns
-# 3) Replace nouns 15% of the time, everything else 10%
+# 2) Pick 5 parts of speech to prompt for, including nouns DOWN
+# 3) Replace nouns 15% of the time, everything else 10% DONE
 
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
 print("START*******")
+
+# code developed by Jackie Cohen; revised by Paul Resnick
+# further revised by Colleen van Lent for Python3
+#sfsgf
+import nltk # requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
+import random
+from nltk.book import *
+from nltk.corpus import gutenberg
+from nltk import word_tokenize,sent_tokenize
+
+tagged_tokens = nltk.pos_tag(text2)
+
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective", "RB":"an adverb"}
+substitution_probabilities = {"NN":.5,"NNS":.5,"VB":.1,"JJ":.1,"RB":.1}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+x =tagged_tokens[:150]
+
+for (word, tag) in tagged_tokens[:150]:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+
+def spacing(listy):
+	lst2 = []
+	for words in listy:
+		if words in [",","?",".","!",":"]:
+			lst2.append(words)
+		else:
+			lst2.append(" "+ words)
+	print("".join(lst2))
+
+og =[]
+print("\n")
+print("ORIGINAL TEXT")
+b = tagged_tokens[:150]
+for thing in b:
+	og.append(thing[0])
+og1 = spacing(og)
+#print(type(og1))
+
+print("\n")
+print("NEW TEXT")
+print("\n")
+print("".join(final_words))
+
 
 
 print("\n\nEND*******")
